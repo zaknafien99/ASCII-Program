ce97a7a6a05c0ed453971ac31641b3619443d44e
nick zervos <zaknafien99@hotmail.com>
2020-09-21 14:55:38 +1000

    Add project files.

---
+++ ASCIIProgram/ASCIIProgram.cpp
@@ -0,0 +1,30 @@
+// ASCIIProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    char c1, c2, c3, c4, c5;
+    std::cout << "Enter 5 letters: ";
+    std::cin >> c1 >> c2 >> c3 >> c4 >> c5;
+    std::cout << "ASCI message: " << int(c1) << " " << int(c2)
+        << " " << int(c3) << " " << int(c4) << " " << int(c5);
+
+    system("pause>0");
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
